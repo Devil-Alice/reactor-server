593b48290b51e645c35c061b23a5a99cd2456b44
Devil-Alice <alice.devil@outlook.com>
2024-12-29 17:36:02 +0800

    将channel_map的实现方式从链表改为数组

--- channel_map.c
+++ channel_map.c
@@ -3,20 +3,34 @@
 #include <stdlib.h>
 #include <unistd.h>
 
-channel_map_t *channel_map_create()
+channel_map_t *channel_map_create(int capacity)
 {
     channel_map_t *channel_map = malloc(sizeof(channel_map_t));
-    channel_map->list = linked_list_create();
+    channel_map->list = calloc(capacity, sizeof(channel_t));
     return channel_map;
+}
+
+int channel_map_expand(channel_map_t *channel_map, int capacity)
+{
+    channel_map->list = realloc(channel_map->list, capacity * sizeof(channel_t));
+    channel_map->capacity = capacity;
+    return 0;
+}
+
+int channel_map_add(channel_map_t *channel_map, int fd, channel_t *channel)
+{
+    //检查数组的空间是否足够存放下标位fd的channel
+    while (fd > (channel_map->capacity -1))
+    {
+        channel_map_expand(channel_map, channel_map->capacity * 2);
+    }
+
+    channel_map->list[fd] = channel;
+    return 0;
 }
 
 void channel_map_destroy(channel_map_t *channel_map)
 {
-    while (1)
-    {
-        linked_list_node_t *node = linked_list_pop_head(&(channel_map->list));
-        free(node->data); // 释放节点的数据，也就是channel
-        if (channel_map->list.head == NULL)
-            break; // 释放完毕，退出
-    }
+    free(channel_map->list);
+    free(channel_map);
 }
--- channel_map.h
+++ channel_map.h
@@ -2,13 +2,18 @@
 #include "channel.h"
 #include "linked_list.h"
 
+//channel_map类是fd与channel的映射，fd即是数组索引，数组的元素就是channel
 typedef struct channel_map
 {
-    //链表，内部存放channel*
-    linked_list_t list;
+    // 数组，内部存放channel*
+    channel_t **list;
+    int capacity;
 } channel_map_t;
 
-channel_map_t *channel_map_create();
+channel_map_t *channel_map_create(int capacity);
+
+int channel_map_expand(channel_map_t *channel_map, int capacity);
+
+int channel_map_add(channel_map_t *channel_map, int fd, channel_t *channel);
 
 void channel_map_destroy(channel_map_t *channel_map);
-
